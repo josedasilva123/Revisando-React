f7f44b7f30165159aba09b1048687e4d16f1c12c
Alex Conder <alex.v.conder@gmail.com>
2021-10-30 11:28:01 -0300

    Página de detalhes

--- next1projeto/src/pages/Main/index.js
+++ next1projeto/src/pages/Main/index.js
@@ -1,116 +1,129 @@
-import React from 'react'
-import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from 'react-icons/fa';
-import {Link} from 'react-router-dom';
-import styles from './index.module.css'
+import React from "react";
+import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import styles from "./index.module.css";
 
-import api from '../../services/api';
+import api from "../../services/api";
 
 const Main = () => {
-    const [loading, setLoading] = React.useState(false);
-    const [repositorio, setRepositorio] = React.useState('');
-    const [repositorios, setRepositorios] = React.useState([]);
-    const [error, setError] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+  const [repositorio, setRepositorio] = React.useState("");
+  const [repositorios, setRepositorios] = React.useState([]);
+  const [error, setError] = React.useState(false);
 
-    // Buscar    
-    React.useEffect(() => {
-        const storageRepositorios = localStorage.getItem('repositorios');
-        if(storageRepositorios){
-            setRepositorios(JSON.parse(storageRepositorios));
-        }        
-    }, []);
-
-    //Salvar Repositorios
-    React.useEffect(() => {
-        localStorage.setItem('repositorios', JSON.stringify(repositorios));
-    }, [repositorios]);
-
-    function handleInputChange(event){
-        setRepositorio(event.target.value);
-        setError(false);
+  // Buscar
+  React.useEffect(() => {
+    const storageRepositorios = localStorage.getItem("repositorios");
+    if (storageRepositorios) {
+      setRepositorios(JSON.parse(storageRepositorios));
     }
-    const handleDelete = React.useCallback((repo) => {
-        const newRepositorios = repositorios.filter(r => r.name !== repo);
-        setRepositorios(newRepositorios);
-    }, [repositorios]);
+  }, []);
 
-    const handleSubmit = React.useCallback((event) => {
-        event.preventDefault();
-        async function submit(){
-            try{
-                setLoading(true);
-                setError(false);
-                if(repositorio === ''){
-                    throw new Error('Você precisar indicar um novo repositório.');
-                }
+  //Salvar Repositorios
+  React.useEffect(() => {
+    localStorage.setItem("repositorios", JSON.stringify(repositorios));
+  }, [repositorios]);
 
-                const response = await api.get(`repos/${repositorio}`);
-                const hasRepo = repositorios.find(repo => repo.name === repositorio);
+  function handleInputChange(event) {
+    setRepositorio(event.target.value);
+    setError(false);
+  }
+  const handleDelete = React.useCallback(
+    (repo) => {
+      const newRepositorios = repositorios.filter((r) => r.name !== repo);
+      setRepositorios(newRepositorios);
+    },
+    [repositorios]
+  );
 
-                if(hasRepo){
-                    throw new Error('Repositório duplicado');    
-                }
-                const data = {
-                    name: response.data.full_name,
-                }
-                console.log(response);
-                setRepositorios([...repositorios, data]);
-                setRepositorio('');                
-            } catch (error) {
-                setError(error);
-            } finally {
-                setLoading(false);
-            }           
+  const handleSubmit = React.useCallback(
+    (event) => {
+      event.preventDefault();
+      async function submit() {
+        try {
+          setLoading(true);
+          setError(false);
+          if (repositorio === "") {
+            throw new Error("Você precisar indicar um novo repositório.");
+          }
+
+          const response = await api.get(`repos/${repositorio}`);
+          const hasRepo = repositorios.find(
+            (repo) => repo.name === repositorio
+          );
+
+          if (hasRepo) {
+            throw new Error("Repositório duplicado");
+          }
+          const data = {
+            name: response.data.full_name,
+          };
+          console.log(response);
+          setRepositorios([...repositorios, data]);
+          setRepositorio("");
+        } catch (error) {
+          setError(error);
+        } finally {
+          setLoading(false);
         }
-        submit();        
-    }, [repositorio, repositorios]);
-        
-    
-    return (
-        <div className={styles.container}>
-            <h1 className={styles.title}>
-                <FaGithub size={25} />
-                Meus Repositórios
-            </h1>
+      }
+      submit();
+    },
+    [repositorio, repositorios]
+  );
 
-            <form className={styles.form + (error ? ' ' + styles.error : '')} onSubmit={handleSubmit}>
-                <input 
-                type="text" 
-                placeholder="Adicionar Repositorios"
-                value={repositorio} 
-                onChange={handleInputChange}
-                />
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <FaGithub size={25} />
+        Meus Repositórios
+      </h1>
 
-                <button type="submit" 
-                className={styles.submitButton + (loading ? ' ' + styles.loading : '')}>
-                    {loading ? (
-                        <FaSpinner size={14} color="#FFF"/>    
-                    ) : (
-                        <FaPlus size={14} color="#FFF"/>
-                    )}                    
-                </button>
-            </form>
+      <form
+        className={styles.form + (error ? " " + styles.error : "")}
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          placeholder="Adicionar Repositorios"
+          value={repositorio}
+          onChange={handleInputChange}
+        />
 
+        <button
+          type="submit"
+          className={
+            styles.submitButton + (loading ? " " + styles.loading : "")
+          }
+        >
+          {loading ? (
+            <FaSpinner size={14} color="#FFF" />
+          ) : (
+            <FaPlus size={14} color="#FFF" />
+          )}
+        </button>
+      </form>
 
-
-            <ul className={styles.list}>
-                {repositorios.map(repo => (
-                    <li key={repo.name}>
-                        <span>
-                            <button className={styles.deleteButton} onClick={() => handleDelete(repo.name)}>
-                                <FaTrash size={14} />
-                            </button>  
-                            {repo.name}
-                        </span>
-                        <Link to="">
-                           <FaBars size={20}/>     
-                        </Link>  
-                    </li>    
-                ))}
-            </ul>
-        </div>
-
-        
-    )
-}
+      <ul className={styles.list}>
+        {repositorios.map((repo) => (
+          <li key={repo.name}>
+            <span>
+              <button
+                className={styles.deleteButton}
+                onClick={() => handleDelete(repo.name)}
+              >
+                <FaTrash size={14} />
+              </button>
+              {repo.name}
+            </span>
+            <Link to={`/repositorio/${encodeURIComponent(repo.name)}`}>
+              <FaBars size={20} />
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Main;
--- next1projeto/src/pages/Repositorio/index.js
+++ next1projeto/src/pages/Repositorio/index.js
@@ -1,11 +1,77 @@
-import React from 'react'
+import React from "react";
+import styles from "./index.module.css";
+import api from "../../services/api";
+import { FaArrowLeft } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
-const Repositorio = () => {
+const Repositorio = ({ match }) => {
+  const [repositorio, setRepositorio] = React.useState({});
+  const [issues, setIssues] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    async function load() {
+      try {
+        const nomeRepo = decodeURIComponent(match.params.repositorio);
+        const [repositorioData, issuesData] = await Promise.all([
+          api.get(`/repos/${nomeRepo}`),
+          api.get(`/repos/${nomeRepo}/issues`, {
+            params: {
+              state: "open",
+              per_page: 5,
+            },
+          }),
+        ]);
+        setRepositorio(repositorioData.data);
+        setIssues(issuesData.data);
+      } catch {
+      } finally {
+        setLoading(false);
+      }
+    }
+    load();
+  }, []);
+  if (loading) {
     return (
-        <div>
-            Repositorio   
+        <div class={styles.loading}>
+            <h1>Carregando...</h1>
         </div>
-    )
-}
+    );
+  } else {
+    return (
+      <div className={styles.container}>
+        <Link to="/" className={styles.backButton}>
+            <FaArrowLeft color="#000" size={30} />
+        </Link>
+        <header className={styles.owner}>
+          <img
+            src={repositorio.owner.avatar_url}
+            alt={repositorio.owner.login}
+          />
+          <h1>{repositorio.name}</h1>
+          <p>{repositorio.description}</p>
+        </header>
+        <div>
+            <ul class={styles.issues}>
+                {issues.map(issue => (
+                 <li key={String(issue.id)}>
+                     <img src={issue.user.avatar_url} alt={issue.user.login} />
+                     <div>
+                         <strong>
+                             <a href={issue.html_url}>{issue.title}</a>
+                             {issue.labels.map(label => (
+                                 <span key={String(label.id)}>{label.name}</span>
+                             ))}
+                         </strong>
+                         <p>{issue.user.login}</p>
+                     </div>
+                 </li>   
+                ))}
+            </ul>
+        </div>
+      </div>
+    );
+  }
+};
 
 export default Repositorio;
